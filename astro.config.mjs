35615dbf55fe6c500fe4312049f5b3057fe7fef6
rizal <masmokhamadrizal@gmail.com>
2026-01-01 23:18:25 +0700

    Update Icon

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,10 @@
 import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
-
+import icon from 'astro-icon';
 // https://astro.build/config
 export default defineConfig({
+  integrations: [icon()],
   vite: {
     plugins: [tailwindcss()]
   }
