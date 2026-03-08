1eb9f8951789e6314c0a04168154ba12db83c385
rizal <masmokhamadrizal@gmail.com>
2026-03-08 14:31:28 +0700

    Add Sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,12 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 import icon from 'astro-icon';
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [icon()],
+  site: 'https://www.rizalproject.my.id', // TODO: Ganti dengan URL domain website Anda
+  integrations: [icon(), sitemap()],
   vite: {
     plugins: [tailwindcss()]
   }
